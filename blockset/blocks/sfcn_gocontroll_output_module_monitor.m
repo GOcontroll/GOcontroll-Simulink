c361ca32f2b90befa3d8c7b80429ec118eb68551
SpieringsAE <maud_spierings@hotmail.com>
2024-09-04 14:14:31 +0200

    fix groundshift datatype, int16 now instead of uint16

--- blockset/blocks/sfcn_gocontroll_output_module_monitor.m
+++ blockset/blocks/sfcn_gocontroll_output_module_monitor.m
@@ -28,7 +28,7 @@
 %%
 %%***************************************************************************************
 function sfcn_gocontroll_output_module_monitor(block)
-	setup(block);
+setup(block);
 end
 
 
@@ -44,99 +44,99 @@ end
 %%   C-Mex counterpart: mdlInitializeSizes
 
 function setup(block)
-	%% Dialog parameters
-	tsamp = 1;
-	% moduleSlot = 2;
-	C1duty = 3;
-	% C2duty = 4;
-	% C3duty = 5;
-	% C4duty = 6;
-	% C5duty = 7;
-	C6duty = 8;
+%% Dialog parameters
+tsamp = 1;
+% moduleSlot = 2;
+C1duty = 3;
+% C2duty = 4;
+% C3duty = 5;
+% C4duty = 6;
+% C5duty = 7;
+C6duty = 8;
 
-	% Number of S-Function parameters expected
-	block.NumDialogPrms     = 8;
-	block.SampleTimes = [block.DialogPrm(tsamp).Data 0];
+% Number of S-Function parameters expected
+block.NumDialogPrms     = 8;
+block.SampleTimes = [block.DialogPrm(tsamp).Data 0];
 
-	%% Register number of input and output ports
-	baseOutputs = 10;
-	block.NumInputPorts = 0;
-	num_duty = 0;
-	for idx = C1duty:C6duty
-		if block.DialogPrm(idx).Data == 1
-			num_duty = num_duty + 1;
-		end
+%% Register number of input and output ports
+baseOutputs = 10;
+block.NumInputPorts = 0;
+num_duty = 0;
+for idx = C1duty:C6duty
+	if block.DialogPrm(idx).Data == 1
+		num_duty = num_duty + 1;
 	end
-	block.NumOutputPorts = baseOutputs+num_duty;
+end
+block.NumOutputPorts = baseOutputs+num_duty;
 
-	moduleTemp = 1;
-	addSimpleOutput(block, moduleTemp, DatatypeID.Int16);
+moduleTemp = 1;
+addSimpleOutput(block, moduleTemp, DatatypeID.Int16);
 
-	moduleGnd = 2;
-	addSimpleOutput(block, moduleGnd, DatatypeID.Uint16);
+moduleGnd = 2;
+addSimpleOutput(block, moduleGnd, DatatypeID.Int16);
 
-	moduleSup = 3;
-	addSimpleOutput(block, moduleSup, DatatypeID.Uint16);
+moduleSup = 3;
+addSimpleOutput(block, moduleSup, DatatypeID.Uint16);
 
-	moduleStat = 4;
-	addSimpleOutput(block, moduleStat, DatatypeID.Uint32);
+moduleStat = 4;
+addSimpleOutput(block, moduleStat, DatatypeID.Uint32);
 
-	moduleChannelsStart = 5;
-	offset = moduleChannelsStart-C1duty; % offset between output port number and dialog parameter number.
-	for idx = C1duty:C6duty
-		addSimpleOutput(block, idx+offset, DatatypeID.Int16);
-		if block.DialogPrm(idx).Data == 1 % this port receives duty cycle feedback
-			offset = offset + 1; % we've added another output port so we need to increase the offset
-			addSimpleOutput(block, idx+offset, DatatypeID.Uint16);
-		end
+moduleChannelsStart = 5;
+offset = moduleChannelsStart-C1duty; % offset between output port number and dialog parameter number.
+for idx = C1duty:C6duty
+	addSimpleOutput(block, idx+offset, DatatypeID.Int16);
+	if block.DialogPrm(idx).Data == 1 % this port receives duty cycle feedback
+		offset = offset + 1; % we've added another output port so we need to increase the offset
+		addSimpleOutput(block, idx+offset, DatatypeID.Uint16);
 	end
-	
-	%% -----------------------------------------------------------------
-	%% Register methods called at run-time
-	%% -----------------------------------------------------------------
+end
 
-	%%
-	%% Start:
-	%%   Functionality    : Called in order to initialize state and work
-	%%                      area values
-	%%   C-Mex counterpart: mdlStart
-	%%
-	block.RegBlockMethod('Start', @Start);
+%% -----------------------------------------------------------------
+%% Register methods called at run-time
+%% -----------------------------------------------------------------
 
-	%%
-	%% Outputs:
-	%%   Functionality    : Called to generate block outputs in
-	%%                      simulation step
-	%%   C-Mex counterpart: mdlOutputs
-	%%
-	block.RegBlockMethod('Outputs', @Outputs);
+%%
+%% Start:
+%%   Functionality    : Called in order to initialize state and work
+%%                      area values
+%%   C-Mex counterpart: mdlStart
+%%
+block.RegBlockMethod('Start', @Start);
 
-	%%
-	%% Update:
-	%%   Functionality    : Called to update discrete states
-	%%                      during simulation step
-	%%   C-Mex counterpart: mdlUpdate
-	%%
-	block.RegBlockMethod('Update', @Update);
+%%
+%% Outputs:
+%%   Functionality    : Called to generate block outputs in
+%%                      simulation step
+%%   C-Mex counterpart: mdlOutputs
+%%
+block.RegBlockMethod('Outputs', @Outputs);
+
+%%
+%% Update:
+%%   Functionality    : Called to update discrete states
+%%                      during simulation step
+%%   C-Mex counterpart: mdlUpdate
+%%
+block.RegBlockMethod('Update', @Update);
 end
 
 function Start(~)
 
-  %% No start
+%% No start
 
 end
 
 
 function Outputs(~)
 
-  %% No output
+%% No output
 
 end
 
 
 function Update(~)
 
-  %% No update
+%% No update
 
 end
 
