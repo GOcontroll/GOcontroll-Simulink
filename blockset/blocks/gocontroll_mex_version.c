286b564a49463b7dc6c0a3a503728ee62c74183e
Maud Spierings <maudspierings@gocontroll.com>
2025-07-10 13:39:57 +0200

    format and remove leftover text from the compile error

--- blockset/blocks/gocontroll_mex_version.c
+++ blockset/blocks/gocontroll_mex_version.c
@@ -2,25 +2,25 @@
 #include "string.h"
 
 #ifndef VERSION
-#error "VERSION must be passed in by command: -DVERSION=<version>"#define VERSION ""
+#error "VERSION must be passed in by command: -DVERSION=<version>"
 #endif
 
 #define stringify_(x) #x
 #define stringify(x) stringify_(x)
 
 /*
-* This function checks if the version that was stored at compile time equals the current version
-* given by the ert_linux_target_version function.
-* Returns true when the mex is up to date, false when it needs to be recompiled
-*/
-void mexFunction( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
+ * This function checks if the version that was stored at compile time equals
+ * the current version given by the ert_linux_target_version function. Returns
+ * true when the mex is up to date, false when it needs to be recompiled
+ */
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 	mxArray *result, *version_actual;
 	char *version_local_str = stringify(VERSION);
 	char *version_actual_str;
 	mexCallMATLAB(1, &version_actual, 0, NULL, "ert_linux_target_version");
 
 	version_actual_str = mxArrayToString(version_actual);
-	if (strcmp(version_actual_str, version_local_str)==0) {
+	if (strcmp(version_actual_str, version_local_str) == 0) {
 		plhs[0] = mxCreateLogicalScalar(true);
 	} else {
 		plhs[0] = mxCreateLogicalScalar(false);
