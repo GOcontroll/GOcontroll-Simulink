be4964d9bacd5735b8fb96fc3d7dce63ac3e13a3
Maud Spierings <maudspierings@gocontroll.com>
2025-11-13 13:40:30 +0100

    make the Main stack memore configurable, can also read heap and stack

---
+++ blockset/blocks/sfcn_ram.m
@@ -0,0 +1,91 @@
+%%***************************************************************************************
+%% file         sfcn_ram.m
+%% brief        Level-2 M file S-Function for the ram configuration on embedded targets
+%%
+%%---------------------------------------------------------------------------------------
+%%                          C O P Y R I G H T
+%%---------------------------------------------------------------------------------------
+%%  Copyright 2024 (c) by GOcontroll      http://www.gocontroll.com     All rights reserved
+%%
+%%---------------------------------------------------------------------------------------
+%%                            L I C E N S E
+%%---------------------------------------------------------------------------------------
+%% Permission is hereby granted, free of charge, to any person obtaining a copy of this
+%% software and associated documentation files (the "Software"), to deal in the Software
+%% without restriction, including without limitation the rights to use, copy, modify, merge,
+%% publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+%% persons to whom the Software is furnished to do so, subject to the following conditions:
+%%
+%% The above copyright notice and this permission notice shall be included in all copies or
+%% substantial portions of the Software.
+%%
+%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+%% INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+%% PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+%% FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+%% OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+%% DEALINGS IN THE SOFTWARE.
+%%
+%%***************************************************************************************
+function sfcn_ram(block)
+setup(block);
+end
+
+%% Function: setup ===================================================
+%% Abstract:
+%%   Set up the S-function block's basic characteristics such as:
+%%   - Input ports
+%%   - Output ports
+%%   - Dialog parameters
+%%   - Options
+%%
+%%   Required         : Yes
+%%   C-Mex counterpart: mdlInitializeSizes
+
+function setup(block)
+%% Register number of input and output ports
+block.NumInputPorts = 0;
+block.NumOutputPorts = 2;
+
+for idx = 1:2
+	addSimpleOutput(block, idx, DatatypeID.Uint32);
+end
+
+block.NumDialogPrms     = 0;
+block.SampleTimes = [-1 0];
+%% -----------------------------------------------------------------
+%% Register methods called at run-time
+%% -----------------------------------------------------------------
+
+%%
+%% Start:
+%%   Functionality    : Called in order to initialize state and work
+%%                      area values
+%%   C-Mex counterpart: mdlStart
+%%
+block.RegBlockMethod('Start', @Start);
+
+%%
+%% Outputs:
+%%   Functionality    : Called to generate block outputs in
+%%                      simulation step
+%%   C-Mex counterpart: mdlOutputs
+%%
+block.RegBlockMethod('Outputs', @Outputs);
+
+end
+
+function Start(~)
+
+%% No start
+
+end
+
+
+function Outputs(~)
+
+%% No output
+
+end
+
+%%******************************* end of sfcn_ram.m **********************
--- blockset/code/iot/Core/Inc/main.h
+++ blockset/code/iot/Core/Inc/main.h
@@ -1,21 +1,21 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.h
-  * @brief          : Header for main.c file.
-  *                   This file contains the common defines of the application.
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.h
+ * @brief          : Header for main.c file.
+ *                   This file contains the common defines of the application.
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2025 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
@@ -41,6 +41,8 @@ extern "C" {
 
 /* Exported constants --------------------------------------------------------*/
 /* USER CODE BEGIN EC */
+
+extern uint32_t main_thread_stack;
 
 /* USER CODE END EC */
 
